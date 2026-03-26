aa4aba0a1f2cd656653a7b85b90e4445f62a3a08
Bianca Eiras <95935490+biaeiras@users.noreply.github.com>
2026-03-26 23:53:20 +0000

    fiz3

--- Aula3-12-03-2026/operacao_bits3.c
+++ Aula3-12-03-2026/operacao_bits3.c
@@ -8,8 +8,17 @@ unsigned char switch_byte(unsigned char x){
     return ((mascara_A >> 4) | (mascara_B << 4));
 }
 
+unsigned char rotate_left(unsigned char x, int deslocamento){
+   
+    return ((x << 4) | (x >> (8-deslocamento)));
+}
+
 int main() {
   unsigned char resultado = switch_byte(0xAB); 
   printf("O resultado é 0x%02x \n", resultado); 
+  unsigned char valor = 0x61; 
+  printf("O rotate é 0x%02x \n", rotate_left(valor, 1)); 
+  printf("O rotate é 0x%02x \n", rotate_left(valor, 2)); 
+  printf("O rotate é 0x%02x \n", rotate_left(valor, 5)); 
   return 0;
 }
