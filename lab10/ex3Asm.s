49fb062f3ff60f619a8e4dcf8f11f3804d872438
Bianca Eiras <95935490+biaeiras@users.noreply.github.com>
2026-05-01 02:00:28 +0000

    ex3

--- lab10/ex3Asm.s
+++ lab10/ex3Asm.s
@@ -22,4 +22,54 @@ Dicionário
 .text
 .globl boo 
     boo: 
-        
+        push %rbp 
+        movq %rsp, %rbp
+        sub $32, %rsp
+
+        #salavr valores callee-saved 
+        movq  %r12, -8(%rbp)
+        movq  %r13, -16(%rbp)
+        movq  %r14d, -24(%rbp)
+
+        #guardando os valores originais dos paramêtros
+
+        movq  %rdi, %r12         
+        movl  %esi, %r13d        
+        movl  %edx, %r14d   
+
+
+    WHILE: 
+        cmpl $0, %r13d 
+        je SAI_DO_WHILE
+
+        #preparando os paramêtros
+        #f(px->val1, val)
+        movl 0(%r12d), %edi
+        movl %r14d, %esi 
+
+        call f
+
+        # px->val2 = f(px->val1, val);
+
+        movl %eax, 4(%r12)
+
+        addq $8, %r12 
+
+        jmp WHILE
+    
+    SAI_DO_WHILE:
+
+        #restaurar os valores
+        movq -8(%rbp), %r12
+        movq -16(%rbp), %r13
+        movq -24(%rbp), %r14d
+
+        movq    %rbp, %rsp  
+        popq    %rbp    
+        ret
+    
+
+
+
+
+
